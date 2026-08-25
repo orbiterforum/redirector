b4ece26c8804655a86306d9e4c1da569ce7e7e2a
Derk Weijers <derk@weijers.xyz>
2026-08-25 11:36:48 +0200

    feat: add pydantic-settings dependency and update settings imports

--- redirector/settings.py
+++ redirector/settings.py
@@ -1,12 +1,12 @@
-from pydantic import BaseSettings, HttpUrl
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     redirect_code: int = 307
-    of_resource_url: HttpUrl = "https://testforumapp.orbithangar.com/resources"
+    of_resource_url: str = "https://testforumapp.orbithangar.com/resources"
     spaces_key: str
     spaces_secret: str
-    spaces_endpoint: HttpUrl = "https://nyc3.digitaloceanspaces.com"
+    spaces_endpoint: str = "https://nyc3.digitaloceanspaces.com"
     space_name: str = "ohm"
     resource_json_location: str = "resources/resources.json"
 
